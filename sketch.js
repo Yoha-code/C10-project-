8a76dec4fe7fbfc786c1f08e76c513796770ef2d
Yoha-code <85884213+Yoha-code@users.noreply.github.com>
2021-06-20 19:42:09 +0530

    Project ship sailing

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var seaImg
+var sea
+var ship
+var shipImg
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+}
+
+
+
+function draw() {
+  background("blue");
+  
+sea.velocityX = -2
+
+  if(sea.x < 0){
+     sea.x = sea.width/2;
+
+  }
+
+  drawSprites();
+}
+
+function setup(){
+  createCanvas(400,400);
+   
+  sea = createSprite(200, 200, 50, 50);
+   sea.addImage(seaImg);
+
+   ship = createSprite(70, 300, 50, 50);
+   ship.addAnimation("sailing", shipImg);
+   ship.scale = 0.2;
+}
+
+
+
+
+
